771b22cdd984ad33b5da18cb33cd11cfb826f52e
Joachim Meyer <jmeyer@boisestate.edu>
2025-08-27 12:39:52 -0600

    Setup - Use Cython as the dedicated version going forward
    With the new setup through toml, we can require having Cython available on the
    target host.

--- setup.py
+++ setup.py
@@ -4,42 +4,27 @@ from pathlib import Path
 
 import numpy
 from setuptools import Extension, setup
-
-try:
-    from Cython.Build import cythonize
-    USE_CYTHON = True
-except Exception:
-    cythonize = None
-    USE_CYTHON = False
-
-print(f"Using Cython {USE_CYTHON}")
-ext_suffix = ".pyx" if USE_CYTHON else ".c"
+from Cython.Distutils import build_ext
 
 # Allow user to specify a compiler; default to gcc if not provided
 os.environ.setdefault("CC", "gcc")
 
-extension_params = dict(
-    extra_compile_args=["-fopenmp", "-O3"],
-    extra_link_args=["-fopenmp", "-O3"],
-    include_dirs=[numpy.get_include()],
-)
-
-loc = Path("topocalc") / "core_c"
-sources = [str(loc / f"topo_core{ext_suffix}"), str(loc / "hor1d.c")]
+source_location = Path("topocalc") / "core_c"
 
 extensions = [
     Extension(
         "topocalc.core_c.topo_core",
-        sources=sources,
-        **extension_params,
+        sources=[
+            str(source_location / f"topo_core.pyx"),
+            str(source_location / "hor1d.c"),
+        ],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args=["-fopenmp", "-O3"],
+        extra_link_args=["-fopenmp", "-O3"],
     )
 ]
 
-if USE_CYTHON:
-    ext_modules = cythonize(
-        extensions, compiler_directives={"language_level": "3"}
-    )
-else:
+setup(
+    cmdclass={"build_ext": build_ext},
     ext_modules = extensions
-
-setup(ext_modules=ext_modules)
+)
