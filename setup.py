afa56e01c563a553a7df776d735758a5148aedbb
Joachim Meyer <jmeyer@boisestate.edu>
2025-08-27 12:17:07 -0600

    Setup - Update to use pyproject.toml and add version.py file
    Update the setup process to use a toml file and address deprecation warnings for
    using outdated setuptools version.

--- setup.py
+++ setup.py
@@ -1,101 +1,45 @@
 #!/usr/bin/env python
-
 import os
+from pathlib import Path
 
 import numpy
-from setuptools import Extension, find_packages, setup
-from setuptools.command.build_ext import build_ext as _build_ext
+from setuptools import Extension, setup
 
-# Test if compiling with cython or using the C source
 try:
-    from Cython.Distutils import build_ext as _build_ext
-except ImportError:
-    USE_CYTHON = False
-else:
+    from Cython.Build import cythonize
     USE_CYTHON = True
+except Exception:
+    cythonize = None
+    USE_CYTHON = False
 
-print('Using Cython {}'.format(USE_CYTHON))
-ext = '.pyx' if USE_CYTHON else '.c'
+print(f"Using Cython {USE_CYTHON}")
+ext_suffix = ".pyx" if USE_CYTHON else ".c"
 
+# Allow user to specify a compiler; default to gcc if not provided
+os.environ.setdefault("CC", "gcc")
 
-class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-
-
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-with open('requirements.txt') as requirements_file:
-    requirements = requirements_file.read()
-
-setup_requirements = ['setuptools_scm']
-
-test_requirements = []
-
-# Give user option to specify their local compiler name
-if "CC" not in os.environ:
-    os.environ["CC"] = "gcc"
-
-# extension parameters
 extension_params = dict(
-    extra_compile_args=['-fopenmp'],
-    extra_link_args=['-fopenmp'],
-    include_dirs=[numpy.get_include()]
+    extra_compile_args=["-fopenmp", "-O3"],
+    extra_link_args=["-fopenmp", "-O3"],
+    include_dirs=[numpy.get_include()],
 )
 
-cmdclass = {'build_ext': build_ext}
-ext_modules = []
+loc = Path("topocalc") / "core_c"
+sources = [str(loc / f"topo_core{ext_suffix}"), str(loc / "hor1d.c")]
 
-# topocalc core c functions
-loc = 'topocalc/core_c'  # location of the folder
-mname = os.path.join(loc, 'topo_core')
-mname = mname.replace('/', '.')
-ext_modules += [
-    Extension(mname,
-              sources=[os.path.join(loc, val) for val in [
-                  "topo_core.pyx",
-                  "hor1d.c",
-              ]],
-              **extension_params,
-              ),
+extensions = [
+    Extension(
+        "topocalc.core_c.topo_core",
+        sources=sources,
+        **extension_params,
+    )
 ]
 
-setup(
-    author="iSnobal Community",
-    python_requires='>=3.6',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
-    ],
-    description="Topo calculations like gradient and sky view",
-    entry_points={
-        'console_scripts': [
-            'topocalc=topocalc.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    include_package_data=True,
-    keywords='topocalc',
-    name='topocalc',
-    packages=find_packages(include=['topocalc', 'topocalc.*']),
-    setup_requires=setup_requirements,
-    test_suite='topocalc.tests',
-    tests_require=test_requirements,
-    cmdclass=cmdclass,
-    ext_modules=ext_modules,
-    url='https://github.com/iSnobal/topocalc',
-    use_scm_version={
-        "local_scheme": "no-local-version"
-    },
-    zip_safe=False,
-)
+if USE_CYTHON:
+    ext_modules = cythonize(
+        extensions, compiler_directives={"language_level": "3"}
+    )
+else:
+    ext_modules = extensions
+
+setup(ext_modules=ext_modules)
