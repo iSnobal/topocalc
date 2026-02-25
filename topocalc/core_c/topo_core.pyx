2949a64d015b146e694715fdbb27acc40865f37b
Joachim Meyer <jmeyer@boisestate.edu>
2026-02-24 17:09:39 -0700

    Setup - Cython - Centralize directives, update to latest build practices
    Chnage the setup.py to use the latest practices for Cython and centralize all custom build
    directives to the file too.

--- topocalc/core_c/topo_core.pyx
+++ topocalc/core_c/topo_core.pyx
@@ -1,10 +1,6 @@
 """
 C implementation of some radiation functions
 """
-# cython: language_level=3
-# cython: boundscheck=False
-# cython: wraparound=False
-# cython: cdivision=True
 # distutils: language = c
 # distutils: define_macros=NPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION
 
